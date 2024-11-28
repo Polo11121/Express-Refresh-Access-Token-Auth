02d346f34c178f2d34a1efb6e70dfa540be2e88d
Polo1121 <michal.jasinski11121@gmail.com>
2024-11-28 23:36:21 +0100

    last test

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const serverless = require("serverless-http"); // Wymagane do uruchomienia na Vercel
+const serverless = require("serverless-http"); // Wymagane do działania na Vercel
 const path = require("path");
 const cors = require("cors");
-const corsOptions = require("../config/corsOptions");
-const { logger } = require("../middleware/logEvents");
-const errorHandler = require("../middleware/errorHandler");
-const verifyJWT = require("../middleware/verifyJWT");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
-const credentials = require("../middleware/credentials");
+const credentials = require("./middleware/credentials");
 const mongoose = require("mongoose");
-const connectDB = require("../config/dbConn");
+const connectDB = require("./config/dbConn");
 
-// Połącz z MongoDB
+// Połączenie z MongoDB
 connectDB();
 
 // Inicjalizacja aplikacji Express
@@ -38,24 +38,24 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Serve static files
-app.use("/", express.static(path.join(__dirname, "../public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
-app.use("/", require("../routes/root"));
-app.use("/register", require("../routes/register"));
-app.use("/auth", require("../routes/auth"));
-app.use("/refresh", require("../routes/refresh"));
-app.use("/logout", require("../routes/logout"));
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 
 app.use(verifyJWT);
-app.use("/employees", require("../routes/api/employees"));
-app.use("/users", require("../routes/api/users"));
+app.use("/employees", require("./routes/api/employees"));
+app.use("/users", require("./routes/api/users"));
 
 // Handle 404 errors
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "../views", "404.html"));
+    res.sendFile(path.join(__dirname, "views", "404.html"));
   } else if (req.accepts("json")) {
     res.json({ error: "404 Not Found" });
   } else {
